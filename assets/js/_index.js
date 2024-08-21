a99b72501d91962d6af0bc65664088976a2d7e6c
Priscilla DigiTech <priscilladigitech@gmail.com>
2024-08-21 06:54:49 +0200

    21/08/24 06:54

--- assets/js/_index.js
+++ assets/js/_index.js
@@ -52,12 +52,21 @@ function closeChat() {
 const qaPairs = [
   {
     question: "Faites-vous des formations sur mesure ?",
-    keywords: ["formations sur mesure", "formation sur mesure"],
+    keywords: [
+      "formations sur mesure",
+      "formation sur mesure",
+      "formation spécifique",
+    ],
     answer: "Oui, nous proposons des formations sur mesure.",
   },
   {
     question: "Faites-vous des préparations aux diplômes ?",
-    keywords: ["préparations aux diplômes", "préparation aux diplômes"],
+    keywords: [
+      "préparations aux diplômes",
+      "préparation aux diplômes",
+      "préparation au diplôme",
+      "préparations au diplôme",
+    ],
     answer: "Oui, nous proposons des préparations aux diplômes.",
   },
   {
@@ -66,6 +75,7 @@ const qaPairs = [
     keywords: [
       "formations préparant à l’attestation de capacité",
       "formation attestation de capacité",
+      "attestation de capacité",
     ],
     answer:
       "Oui, nous proposons des formations préparant à l’attestation de capacité.",
@@ -79,6 +89,7 @@ const qaPairs = [
       "formation à distance",
       "formation en visio",
       "formations en visio",
+      "formation visio",
     ],
     answer: "Oui, nous proposons des formations en distanciel.",
   },
@@ -89,33 +100,47 @@ const qaPairs = [
       "formation en présentiel",
       "formation présentiel",
       "formation sur place",
+      "formation en présence",
+      "formation en entreprise",
+      "formation dans l'entreprise",
     ],
     answer: "Oui, nous proposons des formations en présentiel.",
   },
   {
     question: "Faites-vous des formations dans toute la France ?",
-    keywords: ["formations dans toute la France", "formation France"],
+    keywords: [
+      "formations dans toute la France",
+      "formations dans toute la france",
+      "formation France",
+      "formation france",
+      "dans toute la France",
+      "dans toute la france",
+    ],
     answer: "Oui, nous proposons des formations dans toute la France.",
   },
   {
     question: "Faites-vous les FCO ?",
-    keywords: ["FCO"],
+    keywords: [
+      "FCO",
+      "Formation Continue Obligatoire",
+      "formation continue obligatoire",
+    ],
     answer: "Non, nous ne faisons pas les FCO.",
   },
   {
     question: "Pouvons nous faire un entretien technique ?",
-    keywords: ["entretien technique"],
+    keywords: ["entretien technique", "entretien préalable"],
     answer:
       "Oui, pour demander votre entretien technique veuillez remplir le formulaire ci-dessous :",
   },
   {
     question: "Faites-vous des audits ou diagnostics ?",
-    keywords: ["audits", "diagnostics"],
+    keywords: ["audits", "diagnostics", "audit", "diagnostic"],
     answer: "Oui, nous proposons des audits ou diagnostics.",
   },
   {
     question: "Faites-vous de l’accompagnement ou conseil ?",
-    keywords: ["accompagnement", "conseil"],
+    keywords: ["accompagnement", "conseil", "conseils"],
     answer: "Oui, nous proposons de vous accompagner et de vous conseiller.",
   },
   {
@@ -126,28 +151,35 @@ const qaPairs = [
   },
   {
     question: "Êtes-vous certifié Qualiopi ?",
-    keywords: ["certifié Qualiopi", "Qualiopi"],
+    keywords: [
+      "certifié Qualiopi",
+      "Qualiopi",
+      "qualiopi",
+      "certifié qualiopi",
+    ],
     answer: "Oui, nous sommes certifiés Qualiopi.",
   },
   {
     question: "Quelles sont vos références ?",
-    keywords: ["références", "clients"],
+    keywords: ["références", "clients", "references"],
     answer:
       "Nos références sont des grands groupes comme Carrefour, Fedex, Saint Gobain, mais aussi de nombreuses TPE et PME.",
   },
   {
     question: "Combien de temps durent les formations ?",
-    keywords: ["durée formations", "combien de temps formations"],
+    keywords: [
+      "durée formations",
+      "combien de temps formations",
+      "durée",
+      "temps",
+    ],
     answer:
       "En entreprise, nos formations durent généralement de 1 à 3 jours. Pour les préparations au diplômes, de quelques jours à plusieurs semaines ou mois. Dans tous les cas, nous nous adaptons aux besoins.",
   },
   {
     question:
       "Combien de temps dure une mission de conseil et/ou d’accompagnement ?",
-    keywords: [
-      "durée mission conseil",
-      "combien de temps mission accompagnement",
-    ],
+    keywords: ["mission de conseil", "mission d'accompagnement"],
     answer:
       "Il y a d’abord une phase de diagnostic qui dure de 1 à 5 jours qui permet de déterminer ensuite les besoins et la durée de la mission.",
   },
